f2c0a643cacaf350cc7d9241cf9f328aec902071
Hugo Escandon <hescandon6@gmail.com>
2018-07-12 19:04:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,8 +17,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
       :memorial_day => ["BBQ"]
      }
    }
-   
-end
+
+
 def second_supply_for_fourth_of_july(holiday_supplies)
 holiday_supplies[:summer][:fourth_of_july][1]
   # return the second element in the 4th of July array
